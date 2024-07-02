02073ac25081081d0e47bda013abfa04cf1493c9
Neil Muñoz Lago <164719485+Neiland85@users.noreply.github.com>
2024-07-02 03:12:12 +0200

    Update activate.fish
    Enhance activate.fish script with detailed comments and improvements.

--- {env_name}/bin/activate.fish
+++ {env_name}/bin/activate.fish
@@ -1,8 +1,9 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
 # (https://fishshell.com/). You cannot run it directly.
 
-function deactivate  -d "Exit virtual environment and return to normal shell environment"
-    # reset old environment variables
+# Function to deactivate the virtual environment and return to normal shell environment
+function deactivate -d "Exit virtual environment and return to normal shell environment"
+    # Reset old environment variables
     if test -n "$_OLD_VIRTUAL_PATH"
         set -gx PATH $_OLD_VIRTUAL_PATH
         set -e _OLD_VIRTUAL_PATH
@@ -14,7 +15,7 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
 
     if test -n "$_OLD_FISH_PROMPT_OVERRIDE"
         set -e _OLD_FISH_PROMPT_OVERRIDE
-        # prevents error when using nested fish instances (Issue #93858)
+        # Prevents error when using nested fish instances (Issue #93858)
         if functions -q _old_fish_prompt
             functions -e fish_prompt
             functions -c _old_fish_prompt fish_prompt
@@ -22,6 +23,7 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
         end
     end
 
+    # Unset virtual environment variables
     set -e VIRTUAL_ENV
     set -e VIRTUAL_ENV_PROMPT
     if test "$argv[1]" != "nondestructive"
@@ -30,22 +32,25 @@ function deactivate  -d "Exit virtual environment and return to normal shell env
     end
 end
 
-# Unset irrelevant variables.
+# Unset irrelevant variables before activating new environment
 deactivate nondestructive
 
+# Set the virtual environment path
 set -gx VIRTUAL_ENV "/Users/estudio/Projects/GitHub/Python/catalina-politician-insight-sql/catalina-politician-insight-sql/{env_name}"
 
+# Save the current PATH and update it to include the virtual environment bin directory
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
 
-# Unset PYTHONHOME if set.
+# Unset PYTHONHOME if it is set
 if set -q PYTHONHOME
     set -gx _OLD_VIRTUAL_PYTHONHOME $PYTHONHOME
     set -e PYTHONHOME
 end
 
+# If the prompt is not disabled, override the fish prompt to include the virtual environment name
 if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
-    # fish uses a function instead of an env var to generate the prompt.
+    # Fish uses a function instead of an env var to generate the prompt.
 
     # Save the current fish_prompt function as the function _old_fish_prompt.
     functions -c fish_prompt _old_fish_prompt
@@ -55,7 +60,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         # Save the return status of the last command.
         set -l old_status $status
 
-        # Output the venv prompt; color taken from the blue of the Python logo.
+        # Output the virtual environment prompt; color taken from the blue of the Python logo.
         printf "%s%s%s" (set_color 4B8BBE) "({env_name}) " (set_color normal)
 
         # Restore the return status of the previous command.
@@ -66,4 +71,6 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
 
     set -gx _OLD_FISH_PROMPT_OVERRIDE "$VIRTUAL_ENV"
     set -gx VIRTUAL_ENV_PROMPT "({env_name}) "
+end
+
 end
